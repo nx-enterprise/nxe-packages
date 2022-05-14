0d2843cf4b4a413024d4c79e065273204bcc6868
Ben Snyder <ben@snyder.tech>
2022-05-14 16:26:58 -0400

    fix: add schematics docs

--- .config/schematics.sh
+++ .config/schematics.sh
@@ -3,6 +3,7 @@
 ## creation of packages and apps
 nx g @nx-plus/docusaurus:app docs
 nx g @nrwl/nx-plugin:plugin nx-python
+nx g @nrwl/nx-plugin:plugin nx-python-fastapi
 nx g @nrwl/nx-plugin:plugin nx-php
 nx g @nrwl/nx-plugin:plugin nx-dapr
 nx g @nrwl/nx-plugin:plugin nx-ddd-architecture
