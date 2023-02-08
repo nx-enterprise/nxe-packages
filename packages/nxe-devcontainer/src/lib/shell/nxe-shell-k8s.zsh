329f6e42182b3e3303c5f1a06f4046493c5f08f2
Ben Snyder <ben@snyder.tech>
2023-02-08 06:25:45 +0000

    fix: k8s autoload shell

--- packages/nxe-devcontainer/src/lib/shell/nxe-shell-k8s.zsh
+++ packages/nxe-devcontainer/src/lib/shell/nxe-shell-k8s.zsh
@@ -3,8 +3,9 @@
 alias k="kubectl"
 
 # get the kubeconfig
-export KUBECONFIG="${NXE_WS_DEVCONTAINER}/persist/kubeconfig.yaml"
+export KUBECONFIG=$NXE_WS_DEVCONTAINER/persist/kubeconfig.yaml
 
 # support shell magic
+autoload -U +X compinit && compinit
 source <(kubectl completion zsh) # magic
 source <(helm completion zsh) # magic
